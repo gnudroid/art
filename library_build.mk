6be43280e2bddac2e5e9ba5a2aa57029d838b8ee
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-11-16 14:59:01 +0100

    makefile fixup

--- library_build.mk
+++ library_build.mk
@@ -1,22 +1,28 @@
 
+all-lib : $(LIBRARY_OUT)
+
 $(LIBRARY_OUT):	$(OBJS)
-	@echo "linking $@"
-	@$(CXX) -shared -pthread -Wl,--no-undefined -o $@ $(OBJS) $(LIBS)
+	@echo "linking: $@"
+	@$(CXX) -shared $(LDFLAGS) -Wl,--no-undefined -o $@ $(OBJS) $(LIBS)
+#	@$(CXX) -shared -pthread -Wl,--no-undefined -o $@ $(OBJS) $(LIBS)
 
 %.o : %.c
 %.o : %.c $(DEPDIR)/%.d
-	$(COMPILE.c) $(OUTPUT_OPTION) $<
-	$(POSTCOMPILE)
+        @echo compiling: $@
+	@$(COMPILE.c) $(OUTPUT_OPTION) $<
+	@$(POSTCOMPILE)
 
 %.o : %.cc
 %.o : %.cc $(DEPDIR)/%.d
-	$(COMPILE.cc) $(OUTPUT_OPTION) $<
-	$(POSTCOMPILE)
+	@echo compiling: $@
+	@$(COMPILE.cc) $(OUTPUT_OPTION) $<
+	@$(POSTCOMPILE)
 
 %.o : %.cxx
 %.o : %.cxx $(DEPDIR)/%.d
-	$(COMPILE.cc) $(OUTPUT_OPTION) $<
-	$(POSTCOMPILE)
+	@echo compiling: $@"
+	@$(COMPILE.cc) $(OUTPUT_OPTION) $<
+	@$(POSTCOMPILE)
 
 $(DEPDIR)/%.d: ;
 .PRECIOUS: $(DEPDIR)/%.d

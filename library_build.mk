c5ff36261098f131196fee7038aeb824f566f6bf
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2016-11-16 14:04:48 +0100

    makefile fixup

---
+++ library_build.mk
@@ -0,0 +1,24 @@
+
+$(LIBRARY_OUT):	$(OBJS)
+	@echo "linking $@"
+	@$(CXX) -shared -pthread -Wl,--no-undefined -o $@ $(OBJS) $(LIBS)
+
+%.o : %.c
+%.o : %.c $(DEPDIR)/%.d
+	$(COMPILE.c) $(OUTPUT_OPTION) $<
+	$(POSTCOMPILE)
+
+%.o : %.cc
+%.o : %.cc $(DEPDIR)/%.d
+	$(COMPILE.cc) $(OUTPUT_OPTION) $<
+	$(POSTCOMPILE)
+
+%.o : %.cxx
+%.o : %.cxx $(DEPDIR)/%.d
+	$(COMPILE.cc) $(OUTPUT_OPTION) $<
+	$(POSTCOMPILE)
+
+$(DEPDIR)/%.d: ;
+.PRECIOUS: $(DEPDIR)/%.d
+
+-include $(patsubst %,$(DEPDIR)/%.d,$(basename $(SRCS)))
